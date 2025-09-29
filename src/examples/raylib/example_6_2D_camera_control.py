2397ca4fe642203a88ac4d3636f603911e18b039
hojat72elect <hojat72elect@gmail.com>
2025-09-29 01:40:25 -0400

    Added docs

--- src/examples/raylib/example_6_2D_camera_control.py
+++ src/examples/raylib/example_6_2D_camera_control.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
 
     init_window(1920, 1080, "Example 6 - Camera Control")
 
-    # Load player
-    pos = Vector2()
-    radius = 50
-    direction = Vector2()
-    speed = 400
+    # Load player - Currently, our player is just a black circle.
+    player_position = Vector2()
+    player_radius = 50
+    player_direction = Vector2()
+    player_speed = 400
 
-    # circles
-    circles: list[tuple[Vector2, int, Color]] = [
+    # Draw numerous colorful circles on the screen.
+    circles: list[tuple[Vector2, float, Color]] = [
         (
             Vector2(randint(-2_000, 2_000), randint(-1_000, 1_000)),  # position
             randint(50, 200),  # radius
@@ -28,20 +28,20 @@ if __name__ == '__main__':
     # camera
     camera = Camera2D()
     camera.zoom = 1
-    camera.target = pos
+    camera.target = player_position # Camera's original position is the player
     camera.offset = Vector2(1920 / 2, 1080 / 2)
     camera.rotation = 0
 
     while not window_should_close():
         # Handle keyboard inputs
-        direction.x = int(is_key_down(KEY_RIGHT)) - int(is_key_down(KEY_LEFT))
-        direction.y = int(is_key_down(KEY_DOWN)) - int(is_key_down(KEY_UP))
-        direction = vector2_normalize(direction)
+        player_direction.x = int(is_key_down(KEY_RIGHT)) - int(is_key_down(KEY_LEFT))
+        player_direction.y = int(is_key_down(KEY_DOWN)) - int(is_key_down(KEY_UP))
+        player_direction = vector2_normalize(player_direction)
 
         # movement
         dt = get_frame_time()
-        pos.x += direction.x * (speed * dt)
-        pos.y += direction.y * (speed * dt)
+        player_position.x += player_direction.x * (player_speed * dt)
+        player_position.y += player_direction.y * (player_speed * dt)
 
         # camera update
         rotate_direction = int(is_key_down(KEY_S)) - int(is_key_down(KEY_A))
@@ -50,15 +50,17 @@ if __name__ == '__main__':
         zoom_direction = int(is_key_down(KEY_W)) - int(is_key_down(KEY_Q))
         camera.zoom += (zoom_direction * dt) * 2
         camera.zoom = max(0.2, min(2.0, camera.zoom))
-        camera.target = pos
+        camera.target = player_position
 
         # drawing
         begin_drawing()
         begin_mode_2d(camera)
+
         clear_background(WHITE)
         for circle in circles:
             draw_circle_v(*circle)
-        draw_circle_v(pos, radius, BLACK)
+        draw_circle_v(player_position, player_radius, BLACK)
+
         end_mode_2d()
         end_drawing()
 
