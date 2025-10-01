1244324d70f3cdfd8c36ae29d6fee3bea60f69a4
hojat72elect <hojat72elect@gmail.com>
2025-10-01 03:54:18 -0400

    reformatted the example 10

--- src/examples/raylib/example_10_simple_3D.py
+++ src/examples/raylib/example_10_simple_3D.py
@@ -5,47 +5,50 @@ from pyray import init_window, gen_mesh_cube, load_model_from_mesh, gen_mesh_cyl
 
 from raylib import CAMERA_PERSPECTIVE, MATERIAL_MAP_ALBEDO
 
-init_window(1920, 1080, "3D base")
+if __name__ == '__main__':
 
-# camera
-camera = Camera3D()
-camera.position = Vector3(0, 5.0, 5.0)
-camera.target = Vector3()
-camera.up = Vector3(0, 1, 0)
-camera.fovy = 45.0
-camera.projection = CAMERA_PERSPECTIVE
+    init_window(1920, 1080, "3D base")
 
-# models
-mesh = gen_mesh_cube(1, 1, 1)
-model = load_model_from_mesh(mesh)
+    # 3D enabled camera
+    camera = Camera3D()
+    camera.position = Vector3(0, 5.0, 5.0)
+    camera.target = Vector3()
+    camera.up = Vector3(0, 1, 0)
+    camera.fovy = 45.0
+    camera.projection = CAMERA_PERSPECTIVE
 
-# cylinder
-mesh_cylinder = gen_mesh_cylinder(1, 2, 20)
-model_cylinder = load_model_from_mesh(mesh_cylinder)
+    # Create a cube mesh and make a 3D model out of it.
+    cube_mesh = gen_mesh_cube(1, 1, 1)
+    cube_model = load_model_from_mesh(cube_mesh)
 
-# texture
-image = gen_image_gradient_linear(20, 20, 1, RED, YELLOW)
-texture = load_texture_from_image(image)
-set_material_texture(model_cylinder.materials[0], MATERIAL_MAP_ALBEDO, texture)
+    # Create a cylinder mesh and make a 3D model out of it.
+    cylinder_mesh = gen_mesh_cylinder(1, 2, 20)
+    cylinder_model = load_model_from_mesh(cylinder_mesh)
 
-# move
-pos = Vector3()
-rotation = 0
+    # Create an Image, extract a texture out of it, and apply that texture as the surface material of the model
+    image = gen_image_gradient_linear(20, 20, 1, RED, YELLOW)
+    texture = load_texture_from_image(image)
+    set_material_texture(cylinder_model.materials[0], MATERIAL_MAP_ALBEDO, texture)
 
-while not window_should_close():
-    dt = get_frame_time()
-    # pos.z += 2 * dt
-    rotation += 4 * dt
-    model_cylinder.transform = matrix_rotate_x(rotation)
+    # position and angle of the model in our world.
+    model_position = Vector3()
+    model_rotation = 0
 
-    clear_background(WHITE)
-    begin_drawing()
+    while not window_should_close():
 
-    begin_mode_3d(camera)
-    draw_grid(10, 0.5)
-    draw_model(model_cylinder, pos, 1, WHITE)
-    draw_line_3d(Vector3(-4, 0, -2), Vector3(5, 2, 3), BLACK)
-    end_mode_3d()
+        dt = get_frame_time()
+        model_rotation += 4 * dt
+        cylinder_model.transform = matrix_rotate_x(model_rotation)
 
-    end_drawing()
-close_window()
+        clear_background(WHITE)
+        begin_drawing()
+
+        begin_mode_3d(camera)
+        draw_grid(10, 0.5)
+        draw_model(cylinder_model, model_position, 1, WHITE)
+        draw_line_3d(Vector3(-4, 0, -2), Vector3(5, 2, 3), BLACK)
+        end_mode_3d()
+
+        end_drawing()
+
+    close_window()
